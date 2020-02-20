ddee0868ff0ae72b768923f805f02b9c157db50e
Ig <qvlxty@MacBook-Air-Ig.local>
2020-02-20 18:49:23 +0700

    hotfixes

--- src/implements/modules/user/user.controller.ts
+++ src/implements/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, IRoute, HttpMethod } from "../../../core/base.controller";
 import { Container } from "core/container.class";
 import { UserService } from "./user.service";
+import { Request, Response } from "express";
 
 export class UserController extends Controller {
   constructor(
@@ -14,9 +15,13 @@ export class UserController extends Controller {
     return [
       {
         method: HttpMethod.GET,
-        action: this.userService.getUsers,
+        action: this.getUsers,
         path: "/list"
       }
     ];
+  }
+
+  getUsers = async (req: Request, res: Response) => {
+    res.json(await this.userService.getUsers());
   }
 }
